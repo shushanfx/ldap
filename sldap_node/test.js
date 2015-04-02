b4508647df9f5f7812b1d9b670e0f82187ee4ce4
Daniel Deng <shushanfx@gmail.com>
2015-04-02 12:28:39 +0800

    Update test.js

--- sldap_node/test.js
+++ sldap_node/test.js
@@ -1,14 +1,6 @@
 var sldap = require("./index");
 
-sldap.check("dengjianxin", "shushanfx_D", function(err, msg){
-    if(err){console.error(err); return ;}
-    console.dir(msg);
-});
-sldap.check("dengjianxin", "shushanfx", function(err, msg){
-    if(err){console.error(err); return ;}
-    console.dir(msg);
-});
-sldap.check("shushanfx", "shushanfx", function(err, msg){
+sldap.check("dengjianxin", "dengjianxin", function(err, msg){
     if(err){console.error(err); return ;}
     console.dir(msg);
 });
